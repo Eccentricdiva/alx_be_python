f7b1bbe79834136f2b1a93deea362ef38f22b4ae
Eccentricdiva <osinderotemilola1@gmail.com>
2025-08-10 18:57:41 +0100

    C

--- oop/library_system.py
+++ oop/library_system.py
@@ -5,28 +5,32 @@ class Book:
         self.title = title
         self.author = author
 
+    def __str__(self):
+        return f"Book: '{self.title}' by {self.author}"
+
 class EBook(Book):
     def __init__(self, title, author, file_size):
         super().__init__(title, author)
         self.file_size = file_size  # in MB
+
+    def __str__(self):
+        return f"EBook: '{self.title}' by {self.author}, File Size: {self.file_size}MB"
 
 class PrintBook(Book):
     def __init__(self, title, author, page_count):
         super().__init__(title, author)
         self.page_count = page_count
 
+    def __str__(self):
+        return f"PrintBook: '{self.title}' by {self.author}, Pages: {self.page_count}"
+
 class Library:
     def __init__(self):
-        self.books = []  # list to hold Book/EBook/PrintBook instances
+        self.books = []
 
     def add_book(self, book):
         self.books.append(book)
 
     def list_books(self):
         for book in self.books:
-            if isinstance(book, EBook):
-                print(f"EBook: '{book.title}' by {book.author}, File Size: {book.file_size}MB")
-            elif isinstance(book, PrintBook):
-                print(f"PrintBook: '{book.title}' by {book.author}, Pages: {book.page_count}")
-            else:
-                print(f"Book: '{book.title}' by {book.author}")
+            print(book)  # This will call the book's __str__ method automatically
